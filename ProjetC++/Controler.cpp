522ef409d256e360a8afd656461e28d90db5c12b
Christ <musenga17@gmail.com>
2016-11-26 17:07:42 +0100

    ajout

--- ProjetC++/Controler.cpp
+++
@@ -1,9 +0,0 @@
-#include "Controler.h"
-
-Controler::Controler(Plateau* p) : plateau(p) {}
-
-Controler::~Controler(){}
-
-void Controler::notifyObservable(int indicePlayer){
-  plateau->notifyObserver(indicePlayer);
-}
